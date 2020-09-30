657ea91dcac80b53c31792d3e3ef63a70481bdf7
jplozada <juxnpx@gmail.com>
2020-09-30 10:51:01 -0400

    fill Select of form with useCriptomoneda state data

--- src/hooks/useCriptomoneda.js
+++ src/hooks/useCriptomoneda.js
@@ -22,7 +22,7 @@ const Select = styled.select`
 `;
 
 const useCriptomoneda = (label, stateInicial, opciones) => {
-    console.log(opciones);
+    //console.log(opciones);
     // State de nuestro custom hook
     const [state, actualizarState] = useState(stateInicial);
 
@@ -34,9 +34,9 @@ const useCriptomoneda = (label, stateInicial, opciones) => {
                 value={state}
             >
                 <option value="">- Seleccione - </option>
-                {/* {opciones.map(opcion => (
-                    <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
-                ))} */}
+                {opciones.map(opcion => (
+                    <option key={opcion.CoinInfo.Id} value={opcion.CoinInfo.Name}>{opcion.CoinInfo.FullName}</option>
+                ))}
             </Select>
         </Fragment>
     )
